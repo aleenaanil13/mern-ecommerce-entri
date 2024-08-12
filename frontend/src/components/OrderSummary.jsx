a776ecfd35eee5563d4f3d33530cc86c0e0e2c02
aleenaanil13 <ali13ammu@gmail.com>
2024-08-12 12:00:00 +0300

    Error in createProduct controller Must supply api_key, others all ok

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51Q7Ls7KX40uz8oqaJe9ycfrch13gfRXsaHjIKcvx1DvwKE72Cb88FoxaAijRN42hYUA5eopV01aRfnx6fRVZ2iPU008fQUFLer"
 );
 
 const OrderSummary = () => {
