9bd3dfc64887e6bc0b2d5b19c13183992a87f984
aleenaanil13 <ali13ammu@gmail.com>
2024-08-19 12:00:00 +0300

    adding new category test

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,6 +11,7 @@ const categories = [
 	{ href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
 	{ href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
 	{ href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
+	{ href: "/watches", name: "Watches", imageUrl: "/watches.jpeg" },
 ];
 
 const HomePage = () => {
