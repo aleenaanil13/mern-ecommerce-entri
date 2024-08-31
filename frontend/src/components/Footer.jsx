f20f1c295b720c2a47f0c6d9a4584dff9d4bce35
aleenaanil13 <ali13ammu@gmail.com>
2024-08-31 12:00:00 +0300

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <footer className="bg-gray-600 dark:bg-gray-800 text-gray-800 dark:text-gray-500 py-10">
       <div className="container mx-auto px-4 md:px-8 lg:px-16">
         {/* Top Section: Links and Social Icons */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mb-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mb-8 hidden">
           {/* Column 1: About Us */}
           <div>
             <h3 className="text-white text-xl font-semibold mb-4">About Us</h3>
@@ -78,14 +78,14 @@ const Footer = () => {
         {/* Bottom Section: Copyright */}
         <div className="border-t border-gray-700 pt-6 flex flex-col md:flex-row justify-between items-center text-sm">
           <p>&copy; {new Date().getFullYear()} E-commerce. All rights reserved.</p>
-          <div className="flex space-x-4 mt-4 md:mt-0">
+          {/* <div className="flex space-x-4 mt-4 md:mt-0">
             <Link to="/PrivacyPolicy" onClick={() => handleLinkClick("/privacy-policy")}>
               Privacy Policy
             </Link>
             <Link to="/TermsandConditions" onClick={() => handleLinkClick("/terms-and-conditions") }>
               Terms of Service
             </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </footer>
